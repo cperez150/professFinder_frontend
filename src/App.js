577c030b399820b87f5441e73c845d6b1243772a
Carmen Perez <carmenperez@Carmens-MacBook-Air.local>
2019-12-06 11:55:47 -0500

    fixed image

--- src/App.js
+++ src/App.js
@@ -169,7 +169,7 @@ class App extends Component {
         <header className="header">
           <img
             className="logo"
-            src="/public/images/professFinder.png"
+            src="/images/ProfessFinder.png"
             alt="logo"
           ></img>
           {this.state.showNew ? (
